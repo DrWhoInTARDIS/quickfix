524303583eff4b4ad77d79a71977d8e9b0873594
DrWhoInTARDIS <79606765+DrWhoInTARDIS@users.noreply.github.com>
2021-11-28 20:18:26 -0500

    er

--- code.js
+++ code.js
@@ -1,4 +1,4 @@
-let pass = document.getElementById("password")
+let pass = document.forms["password"]
 pass.addEventListener('submit',(myEvent) => {
     alert(pass.value)
     myEvent.preventDefault()
